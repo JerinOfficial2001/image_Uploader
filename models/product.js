2caff83dbe791571cdfc85c9c202f8a90bea139b
Jerin <78803776+jerin2001@users.noreply.github.com>
2023-07-11 22:04:51 +0530

    shema

--- models/product.js
+++ models/product.js
@@ -4,6 +4,16 @@ const productSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     image: { type: Object, required: true },
+     place: { type: String, required: true },
+    type: { type: String, required: true },
+    price: { type: String, required: true },
+    quantity: { type: String, required: true },
+    timing: { type: String, required: true },
+    address: { type: String, required: true },
+    description: { type: String, required: true },
+    reviews: { type: String, required: true },
+    feedback: { type: String, required: true },
+    rating: { type: String, required: true },
   },
   {
     timestamps: true,
